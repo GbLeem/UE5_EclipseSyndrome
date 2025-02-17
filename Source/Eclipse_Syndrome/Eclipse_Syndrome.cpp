527213bb7f279ee34930e48de1837fafb9b14afd
GBL22M <a01085357914@gmail.com>
2025-02-17 15:47:27 +0900

    빈 프로젝트 생성
    💻init : project file

---
+++ Source/Eclipse_Syndrome/Eclipse_Syndrome.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Eclipse_Syndrome.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Eclipse_Syndrome, "Eclipse_Syndrome" );
