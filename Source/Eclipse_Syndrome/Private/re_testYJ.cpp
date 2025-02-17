906fb46e7a5100ea44f346e9dd216264e0a4326e
Noa <tabacal3590@gmail.com>
2025-02-17 17:07:09 +0900

    기본 test 파일 및 폴더 생성
    🎉 add : test files

---
+++ Source/Eclipse_Syndrome/Private/re_testYJ.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "re_testYJ.h"
+
+// Sets default values
+Are_testYJ::Are_testYJ()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void Are_testYJ::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void Are_testYJ::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/Eclipse_Syndrome/yj_testActor.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "yj_testActor.h"
+
+// Sets default values
+Ayj_testActor::Ayj_testActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void Ayj_testActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void Ayj_testActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
