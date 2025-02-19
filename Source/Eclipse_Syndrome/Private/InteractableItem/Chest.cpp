2b8c59a1f801041a3262988133bf68939ba4b000
Noa <tabacal3590@gmail.com>
2025-02-19 16:13:57 +0900

    아이템 폴더 및 파일 추가 생성 완료
    🎉 add : InteractableItem 폴더 생성 후 내부에 Chest.h, cpp 파일 생성

---
+++ Source/Eclipse_Syndrome/Private/InteractableItem/Chest.cpp
@@ -0,0 +1,23 @@
+#include "InteractableItem/Chest.h"
+
+
+AChest::AChest()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+
+void AChest::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+void AChest::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
