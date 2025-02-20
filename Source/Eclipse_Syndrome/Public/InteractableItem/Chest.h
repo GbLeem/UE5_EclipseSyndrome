8281d4f8f515e7b79a4b6c209e3aedd5d479bab9
Noa <tabacal3590@gmail.com>
2025-02-20 11:23:08 +0900

    chest collision 구현 완료
    ✨ update : (chest) collision 구현
    ✨ update : (chest) player가 범위를 벗어났을 때 chest가 닫히는 애니메이션 추가

--- Source/Eclipse_Syndrome/Public/InteractableItem/Chest.h
+++ Source/Eclipse_Syndrome/Public/InteractableItem/Chest.h
@@ -4,6 +4,11 @@
 #include "GameFramework/Actor.h"
 #include "Chest.generated.h"
 
+
+class UBoxComponent;
+
+
+
 UCLASS()
 class ECLIPSE_SYNDROME_API AChest : public AActor
 {
@@ -12,10 +17,58 @@ class ECLIPSE_SYNDROME_API AChest : public AActor
 public:	
 	AChest();
 
+	UFUNCTION()
+	void OpenChest();
+	UFUNCTION()
+	void CloseChest();
+
+	//if F key, pressed
+	void interact();
+
 protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Chest")
-	USceneComponent* SceneRootComp;
+	UBoxComponent* InteractionBox;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Chest")
 	USkeletalMeshComponent* SkeletalMeshComp;
+
+	UPROPERTY(EditAnywhere, Category = "Animation")
+	UAnimationAsset* OpenAnimation;
+
+
+	//check if player is in range
+	bool bPlayerInRange = false;
+	
+	//Player Overlap
+	UFUNCTION()
+	virtual void OnPlayerOverlapBegin(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	);
+
+	UFUNCTION()
+	virtual void OnPlayerOverlapEnd(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex);
+	
+
+	UFUNCTION()
+	virtual void InteractWithChest();
+
+
+	//Store Random Spawnable Items
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Chest")
+	TArray<TSubclassOf<AActor>> PossibleItems;
+
+
+	//Spawn random item(Bullet / HealItem)
+	void SpawnRandomItem();
+
+
 
 };
